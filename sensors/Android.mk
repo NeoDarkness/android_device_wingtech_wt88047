9e1dd5ab1eea130fb377d19f0efe10bd217d245b
NeoDarkness <aldi.45ckr@gmail.com>
2017-06-11 04:52:41 +0700

    wt88047: Cleanup sensors

--- sensors/Android.mk
+++ sensors/Android.mk
@@ -1,8 +1,19 @@
-ifeq ($(TARGET_DEVICE),wt88047)
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
+LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_C_INCLUDES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
+LOCAL_C_INCLUDES += \
+    external/libxml2/include \
+    external/icu/icu4c/source/common
 
 LOCAL_SRC_FILES := \
     sensors.cpp \
@@ -18,26 +29,14 @@ LOCAL_SRC_FILES := \
     VirtualSensor.cpp \
     sensors_XML.cpp
 
-LOCAL_CFLAGS += -DLOG_TAG=\"Sensors\"
-
-LOCAL_C_INCLUDES := \
-    external/libxml2/include \
-    external/icu/icu4c/source/common
-
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libutils
-
-LOCAL_MODULE := sensors.msm8916
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libcalmodule_common
+
 LOCAL_SRC_FILES := \
     algo/common/common_wrapper.c \
     algo/common/compass/AKFS_AOC.c \
@@ -47,7 +46,6 @@ LOCAL_SRC_FILES := \
 
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_TAGS := optional
-
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 
 include $(BUILD_SHARED_LIBRARY)
@@ -61,5 +59,3 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)
 LOCAL_SRC_FILES := calmodule.cfg
 
 include $(BUILD_PREBUILT)
-
-endif
